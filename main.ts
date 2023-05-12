f7ce296329b453dc824f69a6d48051401827ae2b
Alexander Kasper <alexander.kasper@codecentric.de>
2023-05-12 17:31:13 +0200

    add: random provider

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
 import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
 import { Vpc } from '@cdktf/provider-aws/lib/vpc';
+import { RandomProvider } from '@cdktf/provider-random/lib/provider';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -12,6 +13,8 @@ class MyStack extends TerraformStack {
     new AwsProvider(this, 'aws', {
       region: 'eu-central-1'
     });
+
+    new RandomProvider(this, 'random');
 
     // This is the VPC we want to deploy our resources into
     new Vpc(this, 'vpc', {
