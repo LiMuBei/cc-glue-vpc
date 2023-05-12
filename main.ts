bcb433756ece375ac2d98216c83b0b5842bf48aa
Alexander Kasper <alexander.kasper@codecentric.de>
2023-05-12 17:32:05 +0200

    add: RDS cluster

--- main.ts
+++ main.ts
@@ -4,9 +4,19 @@ import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
 import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
 import { Vpc } from '@cdktf/provider-aws/lib/vpc';
+import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket';
+import { RdsCluster } from '@cdktf/provider-aws/lib/rds-cluster';
+import { SecurityGroup } from '@cdktf/provider-aws/lib/security-group';
+import { SecurityGroupRule } from '@cdktf/provider-aws/lib/security-group-rule';
+import { Password } from '@cdktf/provider-random/lib/password';
+import { SecretsmanagerSecret } from '@cdktf/provider-aws/lib/secretsmanager-secret';
+import { SecretsmanagerSecretVersion } from '@cdktf/provider-aws/lib/secretsmanager-secret-version';
 import { RandomProvider } from '@cdktf/provider-random/lib/provider';
 
 class MyStack extends TerraformStack {
+  readonly vpc: Vpc;
+  readonly rdsCluster: RdsCluster;
+
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
@@ -17,7 +27,7 @@ class MyStack extends TerraformStack {
     new RandomProvider(this, 'random');
 
     // This is the VPC we want to deploy our resources into
-    new Vpc(this, 'vpc', {
+    this.vpc = new Vpc(this, 'vpc', {
       cidrBlock: '10.0.0.0/16'
     });
 
@@ -26,6 +36,99 @@ class MyStack extends TerraformStack {
       bucket: 'glue-bucket',
       acl: 'private'
     });
+
+    // This is the security group the Glue job will use to access the RDS database
+    const glueSecurityGroup = new SecurityGroup(this, 'glue-security-group', {
+      name: 'glue-security-group',
+      description: 'Security group for Glue job'
+    });
+
+    // The RDS cluster
+    this.rdsCluster = this.setupRdsCluster(glueSecurityGroup);
+  }
+
+  private setupRdsCluster(glueSecurityGroup: SecurityGroup) {
+    // This is the security group for the RDS database
+    const rdsSecurityGroup = this.setupRdsSecurity(this.vpc, this, glueSecurityGroup);
+
+    const rdsMasterUsername = 'gluedbadmin';
+    // The RDS master user password
+    const rdsMasterUserPassword = new Password(this, 'rds-master-user-password', {
+      length: 48,
+      special: false
+    });
+    // NOTE: we will be using the master user to access the RDS, DO NOT DO THIS IN PRODUCTION!!!
+
+    // This is the RDS database where the Glue job will write to
+    const rdsCluster = new RdsCluster(this, 'rds-cluster', {
+      clusterIdentifier: 'rds-cluster',
+      engine: 'aurora-postgresql',
+      engineMode: 'provisioned',
+      engineVersion: '15.2',
+      databaseName: 'glue',
+      masterUsername: rdsMasterUsername,
+      masterPassword: rdsMasterUserPassword.result,
+      availabilityZones: ['eu-central-1a', 'eu-central-1b'],
+      vpcSecurityGroupIds: [rdsSecurityGroup.id]
+    });
+
+    // Store master user credentials in secrets manager
+    const rdsMasterUserSecret = new SecretsmanagerSecret(this, 'rds-master-user-secret', {
+      name: 'rds-master-user-secret',
+      description: 'RDS master user secret'
+    });
+    new SecretsmanagerSecretVersion(this, 'rds-master-user-secret-version', {
+      secretId: rdsMasterUserSecret.id,
+      secretString: JSON.stringify({
+        dbClusterIdentifier: rdsCluster.clusterIdentifier,
+        password: rdsMasterUserPassword.result,
+        dbName: rdsCluster.databaseName,
+        port: 5432,
+        host: rdsCluster.endpoint,
+        username: rdsMasterUsername
+      })
+    });
+
+    return rdsCluster;
+  }
+
+  private setupRdsSecurity(vpc: Vpc, scope: Construct, glueSecurityGroup: SecurityGroup) {
+    const rdsSecurityGroup = new SecurityGroup(this, 'rds-security-group', {
+      name: 'rds-security-group',
+      description: 'Security group for RDS database',
+      vpcId: vpc.id
+    });
+
+    // Disallow all outbound traffic from the RDS database
+    new SecurityGroupRule(scope, `rds-disallow-all-outbound-rule`, {
+      securityGroupId: rdsSecurityGroup.id,
+      description: 'Disallow all outbound traffic',
+      type: 'egress',
+      protocol: 'icmp',
+      fromPort: 252,
+      toPort: 86,
+      cidrBlocks: ['255.255.255.255/32']
+    });
+
+    // This is the security ingress group rule allowing access to the RDS database from the Glue job
+    new SecurityGroupRule(this, 'rds-security-group-rule', {
+      type: 'ingress',
+      fromPort: 5432,
+      toPort: 5432,
+      protocol: 'tcp',
+      securityGroupId: rdsSecurityGroup.id,
+      sourceSecurityGroupId: glueSecurityGroup.id
+    });
+    // This is the security group egress rule allowing access to the Glue job from the RDS database
+    new SecurityGroupRule(this, 'glue-security-group-rule', {
+      type: 'egress',
+      fromPort: 5432,
+      toPort: 5432,
+      protocol: 'tcp',
+      securityGroupId: glueSecurityGroup.id,
+      sourceSecurityGroupId: rdsSecurityGroup.id
+    });
+    return rdsSecurityGroup;
   }
 }
 
