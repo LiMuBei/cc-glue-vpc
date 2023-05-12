0816898361fe263152a9905b214ebffec2dd5467
Alexander Kasper <alexander.kasper@codecentric.de>
2023-05-12 11:38:57 +0200

    fix: provider setup

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Construct } from 'constructs';
+import { App, TerraformStack } from 'cdktf';
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    new AwsProvider(this, "aws", {
-      region: "eu-central-1"
+    new AwsProvider(this, 'aws', {
+      region: 'eu-central-1'
+    });
   }
-}
 }
 
 const app = new App();
-new MyStack(app, "glue-vpc-article");
+new MyStack(app, 'glue-vpc-article');
 app.synth();
