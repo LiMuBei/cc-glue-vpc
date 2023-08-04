f39268028310be9526fe5e2f69e2a077b408574b
Alexander Kasper <alexander.kasper@codecentric.de>
2023-08-04 16:57:58 +0200

    chg: setup serverless v2 cluster and fix security group settings

--- main.ts
+++ main.ts
@@ -8,9 +8,6 @@ import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket';
 import { RdsCluster } from '@cdktf/provider-aws/lib/rds-cluster';
 import { SecurityGroup } from '@cdktf/provider-aws/lib/security-group';
 import { SecurityGroupRule } from '@cdktf/provider-aws/lib/security-group-rule';
-import { Password } from '@cdktf/provider-random/lib/password';
-import { SecretsmanagerSecret } from '@cdktf/provider-aws/lib/secretsmanager-secret';
-import { SecretsmanagerSecretVersion } from '@cdktf/provider-aws/lib/secretsmanager-secret-version';
 import { RandomProvider } from '@cdktf/provider-random/lib/provider';
 import { Subnet } from '@cdktf/provider-aws/lib/subnet';
 import { DbSubnetGroup } from '@cdktf/provider-aws/lib/db-subnet-group';
@@ -27,14 +24,9 @@ import { Route } from '@cdktf/provider-aws/lib/route';
 import { RouteTableAssociation } from '@cdktf/provider-aws/lib/route-table-association';
 import { NatGateway } from '@cdktf/provider-aws/lib/nat-gateway';
 import { Eip } from '@cdktf/provider-aws/lib/eip';
+import { RdsClusterInstance } from '@cdktf/provider-aws/lib/rds-cluster-instance';
 
 class MyStack extends TerraformStack {
-  readonly vpc: Vpc;
-  readonly rdsCluster: RdsCluster;
-  readonly rdsClusterSecret: SecretsmanagerSecret;
-  readonly rdsSubnets: Subnet[];
-  readonly glueSubnet: Subnet;
-
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
@@ -44,48 +36,125 @@ class MyStack extends TerraformStack {
 
     new RandomProvider(this, 'random');
 
+    // Setup network
+    const { vpcId, isolatedSubnetA, isolatedSubnetB, privateSubnet } = this.setupNetworking();
+    const { rdsSecurityGroup, glueJobsSecurityGroup } = this.setupSecurityGroups(vpcId);
+
+    // This is the S3 bucket where the Glue job script and resources will live
+    const glueScriptsBucket = new S3Bucket(this, 'glue-bucket', {
+      bucket: 'gluescripts.akasper.codecentric.de'
+    });
+    // Block public access
+    new s3BucketPublicAccessBlock.S3BucketPublicAccessBlock(this, 'glue-bucket-public-access-block', {
+      bucket: glueScriptsBucket.bucket,
+      blockPublicAcls: true,
+      blockPublicPolicy: true,
+      ignorePublicAcls: true,
+      restrictPublicBuckets: true
+    });
+
+    // The RDS master user password
+    const rdsCluster = this.setupRds(rdsSecurityGroup, [isolatedSubnetA.id, isolatedSubnetB.id]);
+
+    // Create connection for Glue jobs to access RDS
+    const rdsConnection = new GlueConnection(this, 'rds-connection', {
+      name: 'rds-connection',
+      connectionProperties: {
+        JDBC_CONNECTION_URL: `jdbc:postgresql://${rdsCluster.endpoint}/${rdsCluster.databaseName}`,
+        JDBC_ENFORCE_SSL: 'true',
+        SECRET_ID: rdsCluster.masterUserSecret.get(0).secretArn
+      },
+      physicalConnectionRequirements: {
+        availabilityZone: privateSubnet.availabilityZone,
+        securityGroupIdList: [glueJobsSecurityGroup.id],
+        subnetId: privateSubnet.id
+      }
+    });
+
+    // The Glue job
+    // Note: we're using the master credentials to access the RDS DB in the job, this is not recommended for production use
+    this.createGlueJob(glueScriptsBucket, rdsCluster.masterUserSecret.get(0).secretArn, rdsConnection);
+  }
+
+  private setupRds(rdsSecurityGroup: SecurityGroup, subnetIds: string[]) {
+    // RDS subnet group
+    const subnetGroup = new DbSubnetGroup(this, 'rds-subnet-group', {
+      name: 'rds-subnet-group',
+      subnetIds: subnetIds
+    });
+
+    // The RDS cluster
+    // This is the RDS cluster where the Glue job will write to
+    const rdsCluster = new RdsCluster(this, 'rds-cluster', {
+      clusterIdentifier: 'rds-cluster',
+      engine: 'aurora-postgresql',
+      engineMode: 'provisioned',
+      engineVersion: '15.3',
+      databaseName: 'glue',
+      masterUsername: 'dbadmin',
+      manageMasterUserPassword: true,
+      vpcSecurityGroupIds: [rdsSecurityGroup.id],
+      dbSubnetGroupName: subnetGroup.name,
+      skipFinalSnapshot: true,
+      serverlessv2ScalingConfiguration: {
+        maxCapacity: 1,
+        minCapacity: 0.5
+      }
+    });
+    // Create RDS instance (single instance, not for production use)
+    new RdsClusterInstance(this, 'rds-cluster-instance', {
+      identifier: 'rds-cluster-instance',
+      clusterIdentifier: rdsCluster.clusterIdentifier,
+      instanceClass: 'db.serverless',
+      engine: rdsCluster.engine,
+      engineVersion: rdsCluster.engineVersion
+    });
+
+    return rdsCluster;
+  }
+
+  private setupNetworking() {
     // This is the VPC we want to deploy our resources into
-    this.vpc = new Vpc(this, 'vpc', {
+    const vpc = new Vpc(this, 'vpc', {
       cidrBlock: '10.0.0.0/16',
       enableDnsHostnames: true,
       enableDnsSupport: true
     });
 
-    // Create isolated subnets for the RDS database
-    const rdsSubnetA = new Subnet(this, 'rds-subnet-a', {
-      vpcId: this.vpc.id,
+    // Create isolated subnets (for the RDS database)
+    const isolatedSubnetA = new Subnet(this, 'isolated-subnet-a', {
+      vpcId: vpc.id,
       cidrBlock: '10.0.1.0/24',
       availabilityZone: 'eu-central-1a',
       mapPublicIpOnLaunch: false,
       tags: {
-        Name: 'rds-subnet-a'
+        Name: 'isolated-subnet-a'
       }
     });
-    const rdsSubnetB = new Subnet(this, 'rds-subnet-b', {
-      vpcId: this.vpc.id,
+    const isolatedSubnetB = new Subnet(this, 'isolated-subnet-b', {
+      vpcId: vpc.id,
       cidrBlock: '10.0.2.0/24',
       availabilityZone: 'eu-central-1b',
       mapPublicIpOnLaunch: false,
       tags: {
-        Name: 'rds-subnet-b'
+        Name: 'isolated-subnet-b'
       }
     });
-    this.rdsSubnets = [rdsSubnetA, rdsSubnetB];
 
-    // Create private subnet for Glue jobs
-    this.glueSubnet = new Subnet(this, 'glue-subnet', {
-      vpcId: this.vpc.id,
+    // Create private subnet (for Glue jobs)
+    const privateSubnet = new Subnet(this, 'private-subnet', {
+      vpcId: vpc.id,
       cidrBlock: '10.0.3.0/24',
       availabilityZone: 'eu-central-1a',
       mapPublicIpOnLaunch: false,
       tags: {
-        Name: 'glue-subnet'
+        Name: 'private-subnet'
       }
     });
 
     // Create a public subnet for the internet gateway and nat gateway
     const publicSubnet = new Subnet(this, 'public-subnet', {
-      vpcId: this.vpc.id,
+      vpcId: vpc.id,
       cidrBlock: '10.0.4.0/24',
       availabilityZone: 'eu-central-1a',
       tags: {
@@ -95,12 +164,12 @@ class MyStack extends TerraformStack {
 
     // Internet gateway for the VPC
     const internetGateway = new InternetGateway(this, 'internet-gateway', {
-      vpcId: this.vpc.id
+      vpcId: vpc.id
     });
 
     // Route traffic from public subnet to internet gateway
     const publicSubnetRouteTable = new RouteTable(this, 'public-subnet-route-table', {
-      vpcId: this.vpc.id
+      vpcId: vpc.id
     });
     new RouteTableAssociation(this, 'public-subnet-route-table-association', {
       routeTableId: publicSubnetRouteTable.id,
@@ -122,168 +191,82 @@ class MyStack extends TerraformStack {
       subnetId: publicSubnet.id
     });
     // Route traffic from glue subnet to NAT gateway
-    const glueSubnetRouteTable = new RouteTable(this, 'glue-subnet-route-table', {
-      vpcId: this.vpc.id
+    const privateSubnetRouteTable = new RouteTable(this, 'private-subnet-route-table', {
+      vpcId: vpc.id
     });
-    new RouteTableAssociation(this, 'glue-subnet-route-table-association', {
-      routeTableId: glueSubnetRouteTable.id,
-      subnetId: this.glueSubnet.id
+    new RouteTableAssociation(this, 'private-subnet-route-table-association', {
+      routeTableId: privateSubnetRouteTable.id,
+      subnetId: privateSubnet.id
     });
-    new Route(this, 'glue-subnet-route', {
-      routeTableId: glueSubnetRouteTable.id,
+    new Route(this, 'private-subnet-route', {
+      routeTableId: privateSubnetRouteTable.id,
       destinationCidrBlock: '0.0.0.0/0',
       natGatewayId: natGateway.id
     });
 
-    // This is the S3 bucket where the Glue job script and resources will live
-    const glueScriptsBucket = new S3Bucket(this, 'glue-bucket', {
-      bucket: 'gluescripts.akasper.codecentric.de'
-    });
-    // Block public access
-    new s3BucketPublicAccessBlock.S3BucketPublicAccessBlock(this, 'glue-bucket-public-access-block', {
-      bucket: glueScriptsBucket.bucket,
-      blockPublicAcls: true,
-      blockPublicPolicy: true,
-      ignorePublicAcls: true,
-      restrictPublicBuckets: true
-    });
-
-    // This is the security group the Glue job will use to access the RDS database
-    const glueSecurityGroup = new SecurityGroup(this, 'glue-security-group', {
-      name: 'glue-security-group',
-      description: 'Security group for Glue job',
-      vpcId: this.vpc.id
-    });
-    // Allow glue security group to access AWS services
-    new SecurityGroupRule(this, 'glue-security-group-egress-aws-services-rule', {
-      type: 'egress',
-      fromPort: 443,
-      toPort: 443,
-      protocol: 'tcp',
-      securityGroupId: glueSecurityGroup.id,
-      cidrBlocks: ['0.0.0.0/0']
-    });
-
-    // The RDS master user password
-    const rdsMasterUsername = 'gluedbadmin';
-    const rdsMasterUserPassword = new Password(this, 'rds-master-user-password', {
-      length: 48,
-      special: false
-    });
-
-    // The RDS cluster
-    this.rdsCluster = this.setupRdsCluster(glueSecurityGroup, rdsMasterUsername, rdsMasterUserPassword.result);
-
-    this.rdsClusterSecret = this.setupRdsClusterSecret(
-      this.rdsCluster,
-      rdsMasterUsername,
-      rdsMasterUserPassword.result
-    );
-
-    // The Glue job
-    this.createGlueJob(glueScriptsBucket, this.rdsCluster, this.rdsClusterSecret, glueSecurityGroup);
+    return {
+      vpcId: vpc.id,
+      isolatedSubnetA: isolatedSubnetA,
+      isolatedSubnetB: isolatedSubnetB,
+      privateSubnet: privateSubnet,
+      publicSubnet: publicSubnet
+    };
   }
 
-  private setupRdsCluster(glueSecurityGroup: SecurityGroup, rdsMasterUsername: string, rdsMasterUserPassword: string) {
-    // This is the security group for the RDS database
-    const rdsSecurityGroup = this.setupRdsSecurity(this.vpc, glueSecurityGroup);
-
-    // NOTE: we will be using the master user to access the RDS, DO NOT DO THIS IN PRODUCTION!!!
-
-    // RDS subnet group
-    const subnetGroup = new DbSubnetGroup(this, 'rds-subnet-group', {
-      name: 'rds-subnet-group',
-      subnetIds: this.rdsSubnets.map((subnet) => subnet.id)
-    });
-
-    // This is the RDS database where the Glue job will write to
-    const rdsCluster = new RdsCluster(this, 'rds-cluster', {
-      clusterIdentifier: 'rds-cluster',
-      engine: 'aurora-postgresql',
-      engineMode: 'provisioned',
-      engineVersion: '15.2',
-      databaseName: 'glue',
-      masterUsername: rdsMasterUsername,
-      masterPassword: rdsMasterUserPassword,
-      vpcSecurityGroupIds: [rdsSecurityGroup.id],
-      dbSubnetGroupName: subnetGroup.name,
-      skipFinalSnapshot: true
-    });
-
-    return rdsCluster;
-  }
-
-  private setupRdsClusterSecret(rdsCluster: RdsCluster, rdsMasterUsername: string, rdsMasterUserPassword: string) {
-    // Store master user credentials in secrets manager
-    const rdsMasterUserSecret = new SecretsmanagerSecret(this, 'rds-master-user-secret', {
-      name: 'rds-master-user-secret-2',
-      description: 'RDS master user secret'
-    });
-    new SecretsmanagerSecretVersion(this, 'rds-master-user-secret-version', {
-      secretId: rdsMasterUserSecret.id,
-      secretString: JSON.stringify({
-        dbClusterIdentifier: rdsCluster.clusterIdentifier,
-        password: rdsMasterUserPassword,
-        dbName: rdsCluster.databaseName,
-        port: 5432,
-        host: rdsCluster.endpoint,
-        username: rdsMasterUsername
-      })
-    });
-
-    return rdsMasterUserSecret;
-  }
-
-  private setupRdsSecurity(vpc: Vpc, glueSecurityGroup: SecurityGroup) {
+  private setupSecurityGroups(vpcId: string) {
+    // This limits the traffic to the RDS database
     const rdsSecurityGroup = new SecurityGroup(this, 'rds-security-group', {
       name: 'rds-security-group',
       description: 'Security group for RDS database',
-      vpcId: vpc.id
+      vpcId: vpcId
     });
 
-    new SecurityGroupRule(this, 'rds-security-group-ingress-rule', {
+    // This limits the traffic to and from the Glue jobs
+    const glueJobsSecurityGroup = new SecurityGroup(this, 'glue-jobs-security-group', {
+      name: 'glue-jobs-security-group',
+      description: 'Security group for Glue jobs',
+      vpcId: vpcId
+    });
+
+    // Allow Glue jobs security group to access RDS security group
+    new SecurityGroupRule(this, 'rds-security-group-ingress', {
       type: 'ingress',
-      fromPort: 5432,
-      toPort: 5432,
-      protocol: 'tcp',
       securityGroupId: rdsSecurityGroup.id,
-      sourceSecurityGroupId: glueSecurityGroup.id
-    });
-    new SecurityGroupRule(this, 'rds-security-group-egress-rule', {
-      type: 'egress',
+      description: 'Allow Glue jobs to access RDS',
       fromPort: 5432,
       toPort: 5432,
       protocol: 'tcp',
-      securityGroupId: glueSecurityGroup.id,
-      sourceSecurityGroupId: rdsSecurityGroup.id
+      sourceSecurityGroupId: glueJobsSecurityGroup.id
     });
 
-    new SecurityGroupRule(this, 'glue-security-group-ingress-rule', {
+    // Allow incoming connections on all ports within the Glue jobs security group
+    // (requirement by Glue)
+    new SecurityGroupRule(this, 'glue-jobs-security-group-ingress', {
       type: 'ingress',
+      securityGroupId: glueJobsSecurityGroup.id,
+      description: 'Allow incoming connections',
       fromPort: 0,
       toPort: 65535,
       protocol: 'tcp',
-      securityGroupId: glueSecurityGroup.id,
-      sourceSecurityGroupId: glueSecurityGroup.id
-    });
-    new SecurityGroupRule(this, 'glue-security-group-egress-rule', {
-      type: 'egress',
-      fromPort: 0,
-      toPort: 65535,
-      protocol: 'tcp',
-      securityGroupId: glueSecurityGroup.id,
-      sourceSecurityGroupId: glueSecurityGroup.id
+      sourceSecurityGroupId: glueJobsSecurityGroup.id
     });
 
-    return rdsSecurityGroup;
+    // Allow Glue jobs security group to access internet
+    // (requirement by Glue, plus allows access to pypi.org for installing Python packages)
+    new SecurityGroupRule(this, 'glue-jobs-security-group-egress', {
+      type: 'egress',
+      securityGroupId: glueJobsSecurityGroup.id,
+      description: 'Allow Glue jobs to access internet',
+      fromPort: 0,
+      toPort: 65535,
+      protocol: 'tcp',
+      cidrBlocks: ['0.0.0.0/0']
+    });
+
+    return { rdsSecurityGroup, glueJobsSecurityGroup };
   }
 
-  private createGlueJob(
-    glueScriptsBucket: S3Bucket,
-    rdsCluster: RdsCluster,
-    rdsMasterUserSecret: SecretsmanagerSecret,
-    glueSecurityGroup: SecurityGroup
-  ) {
+  private createGlueJob(glueScriptsBucket: S3Bucket, rdsMasterUserSecretArn: string, rdsConnection: GlueConnection) {
     const jobPolicyDocument = new DataAwsIamPolicyDocument(this, 'glue-job-policy-document', {
       version: '2012-10-17',
       statement: [
@@ -297,7 +280,7 @@ class MyStack extends TerraformStack {
           sid: 'GlueJobAllowRdsSecretAccess',
           effect: 'Allow',
           actions: ['secretsmanager:GetSecretValue'],
-          resources: [rdsMasterUserSecret.arn]
+          resources: [rdsMasterUserSecretArn]
         }
       ]
     });
@@ -322,20 +305,6 @@ class MyStack extends TerraformStack {
         ]
       }),
       managedPolicyArns: [jobPolicy.arn, 'arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole']
-    });
-
-    const rdsConnection = new GlueConnection(this, 'rds-connection', {
-      name: 'rds-connection',
-      connectionProperties: {
-        JDBC_CONNECTION_URL: `jdbc:postgresql://${rdsCluster.endpoint}/${rdsCluster.databaseName}`,
-        JDBC_ENFORCE_SSL: 'true',
-        SECRET_ID: rdsMasterUserSecret.id
-      },
-      physicalConnectionRequirements: {
-        availabilityZone: this.glueSubnet.availabilityZone,
-        securityGroupIdList: [glueSecurityGroup.id],
-        subnetId: this.glueSubnet.id
-      }
     });
 
     // Upload job script to S3
